6b31eb6f96e1dc8ee734290a557169b350e2a64e
TASLIM RAZA ABDUL HAI ANSARI <148445256+taslim121@users.noreply.github.com>
2025-01-12 23:35:18 +0530

    ............

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://gxrqpnlluwibbmqwkgha.supabase.co';
-const supabaseAnonKey =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4cnFwbmxsdXdpYmJtcXdrZ2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyMjMzNzAsImV4cCI6MjA1MDc5OTM3MH0.BqSZka8wA0LU0vf9wxVqPMlVacpzmnhOzdns1PxTCQo';
+const supabaseUrl = '';
+const supabaseAnonKey =  '';
 
  const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
